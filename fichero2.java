c68f5d792d97f2ce7be9acf16fb3a5bf61453aa5
M4AAAAAAA <llauradofilella@gmail.com>
2023-02-01 23:46:37 +0100

    nananaa

---
+++ fichero2.java
@@ -0,0 +1,12 @@
+package pescamines;
+
+public class soctonto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola 2 mundo!");
+
+	}
+
+}
